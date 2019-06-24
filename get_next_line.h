69f797ee7ec451c64765d789528f13eead38fefb
jadonhansen <jadongavhansen@gmail.com>
2019-06-24 17:19:36 +0200

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: jhansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 14:17:16 by jhansen           #+#    #+#             */
-/*   Updated: 2019/06/24 17:00:08 by jhansen          ###   ########.fr       */
+/*   Updated: 2019/06/24 17:14:53 by jhansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 4
+# define BUFF_SIZE 3
 
 # include "libft/libft.h"
 # include <fcntl.h>
